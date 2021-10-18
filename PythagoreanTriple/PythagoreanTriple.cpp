46b46f65a9507da2198ee1c92e6dfe0d1cc0e515
Giacomo <giacomoravanetti@hotmail.it>
2021-10-18 22:45:28 +0200

    final hypothenuse version

--- PythagoreanTriple/PythagoreanTriple.cpp
+++ PythagoreanTriple/PythagoreanTriple.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<string>
+#include<limits>
+#include<cstdint>
+const double sqrt_2 = std::sqrt(2.);
 
 int main()
 {
@@ -25,21 +28,38 @@ int main()
     if (problem_type == ProblemType::Cathetus) std::cout << "Cathetus\n\r";
 
     std::cout << "Enter an integer ";
-    long number = 0;
+    int_fast64_t number = 0;
     std::cin >> number;
-    const long starting_number = 3;
-    const long starting_sum = starting_number * starting_number;
+    number = (std::numeric_limits<int_fast64_t>::max()>>1)-1;
+    std::cout << "trying with" << number << "\n\r";
+    const int_fast64_t starting_number = 3;
+    const int_fast64_t starting_sum = starting_number * starting_number;
 
     if (problem_type == ProblemType::Hypothenuse) {
-        long hypothenuse_squared = number * number;
-        long A = starting_number;
-        long B = number - 1;
-        long sum = starting_sum+B*B;
-        if (sum == hypothenuse_squared) std::cout << "(" << A << "," << B << "," << number << ") ";
-        for (long add = 1; add < number / 2; ++add) {
-            //if ()
-        }
-    }
+        int_fast64_t hypothenuse_squared = number * number;
+        int_fast64_t B = number - 1;
+        int_fast64_t sum = (starting_sum + 1) - number - number;//= starting_sum + B * B - number*number;
+        uint_fast64_t max_iterations = 0;
+        if (sum == 0) std::cout << "(" << starting_number << "," << B << "," << number << ") ";
+        for (int_fast64_t A = starting_number+1; A<= number/sqrt_2; ++A) {
+            sum += A * 2 - 1;
 
+            uint_fast64_t iterations = 0;
+            while (sum > 0 && B>0) {
+                ++iterations;
+                sum -= --B * 2 + 1;
+            }
+            if (iterations > max_iterations) max_iterations = iterations;
+            
+            if (sum == 0) {
+                if (A * A + B * B == hypothenuse_squared)
+                    std::cout << "(" << A << "," << B << "," << number << ") ";
+                else
+                    std::cout << "Error!\n\r";
+            }
+        }
+        std::cout << "\n\r";
+        std::cout << "max iterations = " << max_iterations << "\n\r";
+    }
     return 0;
 }
